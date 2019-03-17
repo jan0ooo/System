b0d46bb1f43483de88f21d63ec497ca8c1260213
jan0ooo <48594301+jan0ooo@users.noreply.github.com>
2019-03-17 14:13:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2971,7 +2971,7 @@ client.on("guildMemberAdd", member => {
 
 client.on('ready', function(){
     var ms = 10000 ;
-    var setGame = [' !!help ','Malicious™ | Server ' ];
+    var setGame = [' !!help ','Teen | System' ];
     var i = -1;
     var j = 0;
     setInterval(function (){
